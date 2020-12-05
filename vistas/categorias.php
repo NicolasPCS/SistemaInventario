07e3d62cead0e3ecfab41de7f90922f340fcf36b
Nicolas Caytuiro <nicolascaytuirosilva@gmail.com>
2020-12-04 21:04:57 -0500

    Maquetacion del modulo categorias

--- vistas/categorias.php
+++ vistas/categorias.php
@@ -12,9 +12,54 @@
     <?php require_once "menu.php"; ?>
 </head>
 <body>
-    
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-4">
+                <form id="frmCategorias">
+                    <label>Categorias</label>
+                    <input type="text" class="form-control input-sm" name="categoria" id="categoria">
+                    <p></p>
+                    <span class="btn btn-primary" id="btnAgregaCategoria">Agregar</span>
+                </form>
+            </div>
+            <div class="col-sm-6">
+                <div id="tablaCategoriaLoad"></div>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
+
+<script type="text/javascript">
+    $(document).ready(function(){
+
+        $('#tablaCategoriaLoad').load('categorias/tablaCategorias.php');
+
+        $('#btnAgregaCategoria').click(function(){
+
+            vacios = validarFormVacio('frmCategorias');
+
+            if (vacios > 0){
+                alertify.alert("Debes llenar todos los campos!!");
+                return false;
+            }
+
+            datos=$('#frmCategorias').serialize();
+            $.ajax({
+                type:"POST",
+                data:datos,
+                url:"../procesos/categorias/agregaCategoria.php",
+                success:function(r){
+                    if (r == 1){
+                        alertify.success("Categoria agregada con exito");
+                    } else {
+                        alertify.error("No se pudo agregar la categoria");
+                    }
+                }
+            });
+        });
+    });
+</script>
 
 <?php
     } else {
--- vistas/dependencias.php
+++ vistas/dependencias.php
@@ -11,5 +11,6 @@
 <script src="../librerias/alertifyjs/alertify.js"></script>
 <script src="../librerias/bootstrap/js/bootstrap.js"></script>
 <script src="../librerias/select2/js/select2.js"></script>
+<script src="../js/funciones.js"></script>
 <!-- <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script> -->
 <!-- <script src="//code.jquery.com/jquery-1.11.1.min.js"></script> -->
