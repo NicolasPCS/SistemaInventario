16aab32d935635766c51c9dd8d665676f6751795
Nicolas Caytuiro <nicolascaytuirosilva@gmail.com>
2020-12-04 22:00:24 -0500

    Maquetacion del modulo usuarios

--- vistas/usuarios.php
+++ vistas/usuarios.php
@@ -12,9 +12,60 @@
     <?php require_once "menu.php"; ?>
 </head>
 <body>
-    
+    <div class="container">
+        <h1>Administrar Usuarios</h1>
+        <div class="col-sm-4">
+            <form id="frmRegistro">
+                <label>Nombre</label>
+                <input type="text" class="form-control input-sm" name="nombre" id="nombre">
+                <label>Apellido</label>
+                <input type="text" class="form-control input-sm" name="apellido" id="apellido">
+                <label>Usuario</label>
+                <input type="text" class="form-control input-sm" name="usuario" id="usuario">
+                <label>Contraseña</label>
+                <input type="text" class="form-control input-sm" name="password" id="password">
+                <p></p>
+                <span class="btn btn-primary" id="registro">Registrar</span>
+            </form>            
+        </div>
+        <div class="col-sm-7">
+            <div id="tablaUsuariosLoad"></div>
+        </div>
+    </div>
 </body>
 </html>
+
+<script type="text/javascript">
+    $(document).ready(function(){
+        
+        $('#tablaUsuariosLoad').load('usuarios/tablaUsuarios.php');
+
+        $('#registro').click(function(){
+
+            vacios = validarFormVacio('frmRegistro');
+
+            if (vacios > 0){
+                alertify.alert("Debes llenar todos los campos!!");
+                return false;
+            }
+
+            datos=$('#frmRegistro').serialize();
+            $.ajax({
+                type:"POST",
+                data:datos,
+                url:"../procesos/regLogin/registrarUsuario.php",
+                success:function(r){
+                    if (r == 1){
+                        $('#tablaUsuariosLoad').load('usuarios/tablaUsuarios.php');
+                        alertify.alert("Usuario agregado correctamente");
+                    } else {
+                        alertify.error("Fallo al agregar");
+                    }
+                }
+            });
+        });
+    });
+</script>
 
 <?php
     } else {
