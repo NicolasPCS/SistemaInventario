4da22c5869278aced36e4bf1be487f9cdff05b44
Nicolas Caytuiro <nicolascaytuirosilva@gmail.com>
2020-12-05 19:30:15 -0500

    Eliminar cliente

--- clases/clientes.php
+++ clases/clientes.php
@@ -50,5 +50,13 @@
             $sql = "UPDATE clientes set nombre='$datos[1]', apellido='$datos[2]', direccion='$datos[3]', email='$datos[4]', telefono='$datos[5]', rfc='$datos[6]' where id_cliente='$datos[0]'";
             return mysqli_query($conexion,$sql);
         }
+        public function eliminaCliente($idcliente){
+			$c= new conectar();
+			$conexion=$c->conexion();
+
+			$sql="DELETE from clientes where id_cliente='$idcliente'";
+
+			return mysqli_query($conexion,$sql);
+		}
     }
 ?>
